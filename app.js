4102f70669a32b391358ba49ce75d331c4e5ca53
Hamdi Rizal <hamdirizal@gmail.com>
2022-07-12 20:21:22 +0700

    Update

--- app.js
+++ app.js
@@ -1,11 +1,64 @@
+require('dotenv').config()
+const process = require('process')
 const express = require('express')
+const axios = require('axios')
+const fs = require('fs')
+var bodyParser = require('body-parser')
+const { getApplicantDataByExternalId, createAccessToken, getApplicantStatusAndDocs } = require('./sumsub-functions')
 const app = express()
 const port = 3001
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Routes
+app.get('/', (req, res) => { res.send('SumSub Backend!')} );
+app.post('/get-applicant-data', routeGetApplicantData);
+app.post('/create-sumsub-access-token', routeCreateSumsubAccessToken);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+
+async function routeGetApplicantData(req, res){
+  //Send error if externalUserId not provided
+  if(!req || !req.body || !req.body.externalUserId) return res.status(500).send({ error: 'Please provide externalUserId' });
+
+  let applicantId = null;
+
+  try {
+    const res1 = await axios.request(getApplicantDataByExternalId(req.body.externalUserId))
+    applicantId = res1.data.id;
+  } catch (error1) {
+    return res.status(500).send({ error: 'Cannot find applicant with this external-id' })  
+  }
+
+  try{
+    console.log('trying to get res2')
+    const res2 = await axios.request(getApplicantStatusAndDocs(applicantId));
+    return res.json(res2.data)
+  }
+  catch(error2){
+    console.log(error2)
+    return res.status(500).send({ error: 'Failed to get the status and documents' })  
+  }
+}
+
+
+
+async function routeCreateSumsubAccessToken(req, res){
+  //Send error if externalUserId not provided
+  if(!req || !req.body || !req.body.externalUserId) return res.status(500).send({ error: 'Please provide externalUserId' });
+
+  try {
+    //Try to create applicant. If success, hold the applicantId;
+    const res1 = await axios.request(createAccessToken(req.body.externalUserId))
+    return res.json(res1.data.token)    
+  } catch (error1) {
+    console.log(req.body.externalUserId)
+    return res.status(500).send({ error: 'Cannot create access token' })
+  }
+}
+
+
+
+
+app.listen(port, () => { console.log(`Example app listening on port ${port}`) })
---
+++ sumsub-functions.js
@@ -0,0 +1,148 @@
+require('dotenv').config()
+const process = require('process')
+const axios = require('axios');
+const crypto = require('crypto');
+const fs = require('fs');
+const FormData = require('form-data');
+
+const DOWNLOAD_FOLDER = './_downloads/';
+const SUMSUB_BASE_URL = 'https://api.sumsub.com'; 
+
+var config = {};
+config.baseURL= SUMSUB_BASE_URL;
+
+axios.interceptors.request.use(interceptorCreateSignature, function (error) {
+  return Promise.reject(error);
+});
+
+/**
+ * This interceptor will create signature for the request 
+ * The explanation is here: https://developers.sumsub.com/api-reference/#app-tokens
+ */
+function interceptorCreateSignature(config) {
+  console.log('creating signature')
+  var ts = Math.floor(Date.now() / 1000);
+  const signature = crypto.createHmac('sha256',  process.env.SUMSUB_SECRET_KEY);
+  signature.update(ts + config.method.toUpperCase() + config.url);
+
+  if (config.data instanceof FormData) {
+    signature.update (config.data.getBuffer());
+  } else if (config.data) {
+    signature.update (config.data);
+  }
+
+  config.headers['X-App-Access-Ts'] = ts;
+  config.headers['X-App-Access-Sig'] = signature.digest('hex');
+
+  return config;
+}
+
+
+
+/**
+ * This function will try to create an applicant in the Sumsub backend. 
+ * If applicant with same userId exists, it will return error code 409 and gives the exisiting applicant id.
+ * If applicant not exists, it will create a new one, and return and object which contains the new applicant id.
+ * @param {Object} requestBody Must have externalUserId and can have optional "phone" and "email" for prefilling the form.
+ */
+ function createApplicant(requestBody) {
+  console.log('creating applicant')
+  var method = 'post';
+  var url = '/resources/applicants?levelName=' + process.env.SUMSUB_VERIFICATION_LEVEL;
+  var ts = Math.floor(Date.now() / 1000);
+
+  var headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-App-Token': process.env.SUMSUB_APP_TOKEN
+  };
+
+  config.method = method;
+  config.url = url;
+  config.headers = headers;
+  config.data = JSON.stringify(requestBody);
+
+  return config;
+}
+
+
+/**
+ * Getting the aplicant by external user id. 
+ * If applicant exist, it will return object which contains applicant id. Otherwise, it will response with 404.
+ * @param {string} externalUserId Any unique key to difrentiate users.
+ */
+function getApplicantDataByExternalId(externalUserId){  
+  var method = 'get';
+  var url = '/resources/applicants/-;externalUserId='+externalUserId+'/one';    
+
+  var headers = {
+    'Accept': 'application/json',
+    'X-App-Token': process.env.SUMSUB_APP_TOKEN
+  };
+
+  config.method = method;
+  config.url = url;
+  config.headers = headers;
+  config.data = null
+
+  return config;
+}
+
+
+
+/**
+ * Getting the status and document of a particular applicant
+ * @param {*} applicantId SumSub applicant id
+ */
+function getApplicantStatusAndDocs(applicantId){  
+  console.log("Getting aplicant status");
+  
+    var method = 'get';
+    var url = '/resources/applicants/'+applicantId+'/requiredIdDocsStatus';    
+  
+    var headers = {
+      'Accept': 'application/json',
+      'X-App-Token': process.env.SUMSUB_APP_TOKEN
+    };
+  
+    config.method = method;
+    config.url = url;
+    config.headers = headers;
+    config.data = null
+  
+    return config;
+}
+
+
+
+
+/**
+ * Getting the access token which is needed to render the form.
+ * https://developers.sumsub.com/api-reference/#access-tokens-for-sdks
+ * The access token will look like this _act-sbx-b1bee9ed-e57c-4da0-8ada-8e6459093131
+ * @param {String} externalUserId The user id which will be used to pick the applicant
+ */
+ function createAccessToken (externalUserId) {
+  const ttlInSecs = 60 * 15; //Lifespan of a token in seconds. 15 minutes
+  console.log("Creating an access token for initializng SDK...");
+
+  var method = 'post';
+  var url = `/resources/accessTokens?userId=${externalUserId}&ttlInSecs=${ttlInSecs}&levelName=${process.env.SUMSUB_VERIFICATION_LEVEL}`;
+
+  var headers = {
+      'Accept': 'application/json',
+      'X-App-Token': process.env.SUMSUB_APP_TOKEN
+  };
+
+  config.method = method;
+  config.url = url;
+  config.headers = headers;
+  config.data = null;
+
+  return config;
+}
+
+
+
+
+module.exports = {createApplicant, getApplicantDataByExternalId, createAccessToken, getApplicantStatusAndDocs}
