b7f0fd6c3292536c14d0e0e66a73c547b710aa14
Hamdi Rizal <hamdirizal@gmail.com>
2022-08-25 19:46:38 +0700

    Added more mockdata

--- app.js
+++ app.js
@@ -217,6 +217,17 @@ app.all('/account/deposits/automat', (req,res)=>{
 });
 
 
+app.all('/account/withdraw', (req,res)=>{
+  let obj = {success:true, id:1, amount: 300, scheduled_on: "2022-12-12 00:00:00", asset_transfer_id: '', status_id: 0, disbursable: 0, unit_count: 0, price_per_unit: 0, asset_name: 'USDC', client: 0, is_automat: false, protocol: 0, address: '', settled_at: "2022-12-12 00:00:00", created_at: "2022-12-12 00:00:00", completed_at: "2022-12-12 00:00:00", }
+  return res.json(obj);
+});
+
+app.all('/account/withdraw/balance', (req,res)=>{
+  let obj = {balance: 2000,available_balance: 1500 }
+  return res.json(obj);
+});
+
+
 
 
 app.all('/account/funds', (req,res)=>{
