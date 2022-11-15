e10c4a9c0650a094a02000219091f4ba30c69c7a
Hamdi Rizal <hamdirizal@gmail.com>
2022-11-15 16:06:06 +0700

    Update

--- all__account__setupstate.js
+++ all__account__setupstate.js
@@ -1,8 +1,26 @@
 const all__account__setupstate = (req,res)=>{
-  //Sumsub states: init, completed, pending
-  //PrimeTrust states: null, pending, opened
-  let obj = {"business_verification":"NONE","submit_documentation":0,"deposit_status":0,"connect_accounts":true,"details":[], sumsub_state: 'completed', pt_state: 'opened'}
-  // let obj = {"business_verification":"NONE","submit_documentation":0,"deposit_status":0,"details":[],"connect_accounts":false}
+  let obj = {
+    "business_verification":"VERIFIED",
+    "connect_accounts":false,
+    "submit_documentation":0,"details":[],"sumsub_state":"completed","pt_state":"opened","deposit_status":1
+  }
   return res.json(obj);
 }
 module.exports = {all__account__setupstate}
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //Sumsub states: init, completed, pending
+  //PrimeTrust states: null, pending, opened
+  // let obj = {"business_verification":"NONE","submit_documentation":0,"deposit_status":0,"connect_accounts":true,"details":[], sumsub_state: 'completed', pt_state: 'opened'}
+  // let obj = {"business_verification":"NONE","submit_documentation":0,"deposit_status":0,"details":[],"connect_accounts":false}
