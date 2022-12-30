1289309c6b6d7a45bec8b6b40fe374198b627a27
Hamdi Rizal <hamdirizal@gmail.com>
2022-12-30 13:58:12 +0700

    Update

--- all__pools__all.js
+++ all__pools__all.js
@@ -1,5 +1,7 @@
 const all__pools__all = (req,res)=>{
-  const obj = [{"id":999,"pool_name":"Hamdi Cool","pool_description":null,"pool_template_id":0,"created_at":"2022-10-19T04:15:24.308Z","balance":0,"deposit_status":0,"status_id":"1","automated_transfer":0,"apy":0},{"id":226,"pool_name":"Hamdi Cool 2","pool_description":null,"pool_template_id":0,"created_at":"2022-10-19T04:20:31.623Z","balance":0,"deposit_status":0,"status_id":"1","automated_transfer":0,"apy":0},{"id":227,"pool_name":"Hamdi Cool 3","pool_description":null,"pool_template_id":0,"created_at":"2022-10-19T04:27:51.098Z","balance":0,"deposit_status":0,"status_id":"1","automated_transfer":0,"apy":0}]
-  return res.json(obj);
+  const obj = [{"id":999,"pool_name":"Hamdi Cool","pool_description":null,"pool_template_id":0,"created_at":"2022-10-19T04:15:24.308Z","balance":100,"deposit_status":0,"status_id":"1","automated_transfer":0,"apy":3},{"id":226,"pool_name":"Hamdi Cool 2","pool_description":null,"pool_template_id":0,"created_at":"2022-10-19T04:20:31.623Z","balance":0,"deposit_status":0,"status_id":"1","automated_transfer":0,"apy":0},{"id":227,"pool_name":"Hamdi Cool 3","pool_description":null,"pool_template_id":0,"created_at":"2022-10-19T04:27:51.098Z","balance":25000,"deposit_status":0,"status_id":"1","automated_transfer":0,"apy":2}]
+  return res.json(obj);  
+  // return res.json({}); //Empty data
+  // return res.sendStatus(400); //Failure
 }
 module.exports = {all__pools__all}
