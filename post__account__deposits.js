75507c986160554267e7ad427dd5a6556452015f
Hamdi Rizal <hamdirizal@gmail.com>
2023-01-03 08:57:11 +0700

    Update

--- post__account__deposits.js
+++ post__account__deposits.js
@@ -1,5 +1,9 @@
 const post__account__deposits = (req,res)=>{
   let obj = {success:true, message:"Mock data: The deposit is created."}
-  return res.json(obj);
+  setTimeout(() => {    
+    return res.json(obj); 
+    // return res.json({}); //Empty data
+    // return res.status(401).json({ "statusCode": 401, "message": "Mock: Deposit failed", "error": "Error" });
+  }, 2000);
 }
 module.exports = {post__account__deposits}
